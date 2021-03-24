275b75bdb408b37972d7012a8786dadd378e3ee9
Mangesh Yadav <mangeshsyadav@gmail.com>
2021-03-24 18:31:28 +0000

    Added entity analysis

--- language_api/main.py
+++ language_api/main.py
@@ -31,10 +31,9 @@ def homepage():
 @app.route("/upload", methods=["GET", "POST"])
 def upload_text():
     text = request.form["text"]
-
     # Analyse sentiment using Sentiment API call
     sentiment = analyze_text_sentiment(text)[0].get('sentiment score')
-
+    entity_analysis = gcp_analyze_entities(text)
     # Assign a label based on the score
     overall_sentiment = 'unknown'
     if sentiment > 0:
@@ -54,17 +53,14 @@ def upload_text():
     kind = "Sentences"
 
     # Create the Cloud Datastore key for the new entity.
-    key = datastore_client.key(kind, 'sample_task')
-
-    # Alternative to above, the following would store a history of all previous requests as no key
-    # identifier is specified, only a 'kind'. Datastore automatically provisions numeric ids.
-    # key = datastore_client.key(kind)
+    key = datastore_client.key(kind)
 
     # Construct the new entity using the key. Set dictionary values for entity
     entity = datastore.Entity(key)
     entity["text"] = text
     entity["timestamp"] = current_datetime
     entity["sentiment"] = overall_sentiment
+    entity["entity_analysis"] = entity_analysis
 
     # Save the new entity to Datastore.
     datastore_client.put(entity)
@@ -112,7 +108,63 @@ def analyze_text_sentiment(text):
     return sentence_sentiment
 
 
+def gcp_analyze_entities(text, debug=0):
+    """
+    Analyzing Entities in a String
+
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language.LanguageServiceClient()
+    document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
+    response = client.analyze_entities(document=document)
+    output = []
+
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        item = {}
+        item["name"] = entity.name
+        item["type"] = language.Entity.Type(entity.type_).name
+        item["Salience"] = entity.salience
+
+        if debug:
+            print(u"Representative name for the entity: {}".format(entity.name))
+
+            # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+            print(u"Entity type: {}".format(language.Entity.Type(entity.type_).name))
+
+            # Get the salience score associated with the entity in the [0, 1.0] range
+            print(u"Salience score: {}".format(entity.salience))
+
+        # Loop over the metadata associated with entity. For many known entities,
+        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+        # Some entity types may have additional metadata, e.g. ADDRESS entities
+        # may have metadata for the address street_name, postal_code, et al.
+        for metadata_name, metadata_value in entity.metadata.items():
+            item[metadata_name] = metadata_value
+            if debug:
+                print(u"{}: {}".format(metadata_name, metadata_value))
+
+        # Loop over the mentions of this entity in the input document.
+        # The API currently supports proper noun mentions.
+        if debug:
+            for mention in entity.mentions:
+                print(u"Mention text: {}".format(mention.text.content))
+                # Get the mention type, e.g. PROPER for proper noun
+                print(
+                    u"Mention type: {}".format(language.EntityMention.Type(mention.type_).name)
+                )
+        output.append(item)
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    if debug:
+        print(u"Language of the text: {}".format(response.language))
+
+    return (output)
 if __name__ == "__main__":
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8085, debug=True)
